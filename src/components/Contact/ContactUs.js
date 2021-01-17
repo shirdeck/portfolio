ba8a45ca16a901a3696a865b02c7c782cfc74f4b
shirdeck <shirdecker@gmail.com>
2021-01-17 20:47:41 +0200

    fixed minor bugs

--- src/components/Contact/ContactUs.js
+++ src/components/Contact/ContactUs.js
@@ -1,7 +1,21 @@
 import React from "react";
 import emailjs from "emailjs-com";
+import useSound from 'use-sound';
+import soundUrl from "../../oops.mp3";
 
 export default function ContactUs() {
+
+  // eslint-disable-next-line
+  const [play, { stop }] = useSound(
+    soundUrl,
+    { volume: 0.5 }
+  );
+
+  // eslint-disable-next-line
+  const [isHovering, setIsHovering] = React.useState(
+    false
+  );
+
   function sendEmail(e) {
     e.preventDefault();
 
@@ -24,15 +38,20 @@ export default function ContactUs() {
 
   return (
     <form className="contact-form" onSubmit={sendEmail}>
+      <div className="headline">
       <p>!let's talk!</p>
+      </div>
       <div className="grid">
         <div className="head">
-        <input type="text" name="user_name" placeholder="NAME" />
-            <input type="email" name="user_email" placeholder="EMAIL" />
+        <input type="text" name="user_name" placeholder="NAME" required />
+            <input type="email" name="user_email" placeholder="EMAIL" required />
         </div>
         <div className="body">
-        <textarea placeholder="HEY SHIR..." name="message" />
-          <input type="submit" value="SEND" />
+        <textarea placeholder="HEY SHIR..." name="message" required />
+          <input type="submit" value="SEND" onMouseDown={() => {
+        setIsHovering(true);
+        play();
+      }}/>
         </div>
       </div>
     </form>
