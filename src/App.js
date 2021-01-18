f6174620e749c5e8ad30de0fe6e40c47412a8aef
shirdeck <shirdecker@gmail.com>
2021-01-18 15:08:58 +0200

    added about modal

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import ContactUs from './components/Contact/ContactUs';
 import Links from './components/Links/Links'
 import WebIcon from "./images/web-icon.png";
 import Memoji from "./images/memoji.png";
+import Modal from "./components/Modal/Modal"
 
 function App() {
+
   return (
     <div className="grid-container">
       <div className="logo">
@@ -15,7 +17,8 @@ function App() {
       <div className="title">
         <span>shir decker : web developer</span>
       </div>
-      <div className="about-title">About Me</div>
+      <Modal/>
+      {/* <div className="about-title" onClick={handleOpen}>About Me</div> */}
       <div className="projects-title">Projects</div>
       <div className="jff-title">Just For Fun</div>
       <div className="ticker">
